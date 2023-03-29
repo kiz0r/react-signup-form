f6d8fc035f02711777e6e840f940d7161df4fb2a
kiz0r <kkhomenko10@gmail.com>
2023-03-29 22:40:04 +0200

    create sign up form

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -19,8 +19,14 @@ const {
 const INITIAL_VALUES = {
   username: '',
   password: '',
-  repeatedPassword: '',
+  confirmedPassword: '',
   email: '',
+};
+
+const SIGNUP_FORM_REG_EXPS = {
+  username: /^[^._ ](?:[\w-]|\.[\w-])+[^._ ]$/,
+  email: /^.+@.+\.[a-z]{2,3}$/,
+  password: /^(?=.*[A-Z].*)(?=.*[a-z].*)(?=.*\d.*)(?=.*[!@#$%^&*.].*).{8,20}$/,
 };
 
 class SignUpForm extends Component {
@@ -30,32 +36,101 @@ class SignUpForm extends Component {
     this.state = {
       username: INITIAL_VALUES.username,
       password: INITIAL_VALUES.password,
-      repeatedPassword: INITIAL_VALUES.repeatedPassword,
+      confirmedPassword: INITIAL_VALUES.repeatedPassword,
       email: INITIAL_VALUES.email,
+      isUsernameValid: false,
+      isConfirmed: false,
       isEmailValid: false,
       isPasswordValid: false,
+      isChecked: false,
     };
   }
 
+  handleUsernameChange = ({ target: { value } }) => {
+    this.setState({
+      username: value,
+      isUsernameValid: SIGNUP_FORM_REG_EXPS.username.test(value),
+    });
+  };
+
+  handleEmailChange = ({ target: { value } }) => {
+    this.setState({
+      email: value,
+      isEmailValid: SIGNUP_FORM_REG_EXPS.email.test(value),
+    });
+  };
+
+  handlePasswordChange = ({ target: { value } }) => {
+    this.setState({
+      password: value,
+      isPasswordValid: SIGNUP_FORM_REG_EXPS.password.test(value),
+    });
+  };
+
+  handleConfirmPasswordChange = ({ target: { value } }) => {
+    this.setState({
+      confirmedPassword: value,
+      isConfirmed:
+        this.state.password === value &&
+        SIGNUP_FORM_REG_EXPS.password.test(value),
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // request
+  };
+
+  handleChecking = ({ target: { checked } }) => {
+    this.setState({ isChecked: checked });
+  };
+
   render() {
-    const { isEmailValid, isPasswordValid } = this.state;
+    const {
+      username,
+      email,
+      password,
+      confirmedPassword,
+      isUsernameValid,
+      isEmailValid,
+      isPasswordValid,
+      isConfirmed,
+      isCheked,
+    } = this.state;
+
+    const usernameClassName = classNames(
+      `${formInput}`,
+      `${isUsernameValid ? inputValid : inputInvalid}`
+    );
 
     const emailClassName = classNames(
       `${formInput}`,
       `${isEmailValid ? inputValid : inputInvalid}`
     );
 
+    const passwordClassName = classNames(
+      `${formInput}`,
+      `${isPasswordValid ? inputValid : inputInvalid}`
+    );
+
+    const confirmedPassClassName = classNames(
+      `${formInput}`,
+      `${isConfirmed ? inputValid : inputInvalid}`
+    );
+
     return (
       <div className={formWrapper}>
-        <h1 className={formTitle}>Sing Up Form</h1>
-        <form action="" className={signUpForm}>
+        <h1 className={formTitle}>Sign Up Form</h1>
+        <form action="" className={signUpForm} onSubmit={this.handleSubmit}>
           <label htmlFor="" className={formLabel}>
             <span className={labelName}>USERNAME</span>
             <input
               type="text"
-              className={formInput}
+              className={usernameClassName}
               placeholder="Username"
+              value={username}
               name="username"
+              onChange={this.handleUsernameChange}
               autoFocus
               required
             />
@@ -64,18 +139,22 @@ class SignUpForm extends Component {
             <span className={labelName}>PASSWORD</span>
             <input
               type="password"
-              className={formInput}
+              className={passwordClassName}
+              value={password}
               name="password"
               required
+              onChange={this.handlePasswordChange}
             />
           </label>
           <label htmlFor="" className={formLabel}>
             <span className={labelName}>CONFIRM PASSWORD</span>
             <input
               type="password"
-              className={formInput}
+              className={confirmedPassClassName}
+              value={confirmedPassword}
               name="password-confirmation"
               required
+              onChange={this.handleConfirmPasswordChange}
             />
           </label>
           <label htmlFor="" className={formLabel}>
@@ -84,12 +163,20 @@ class SignUpForm extends Component {
               type="email"
               className={emailClassName}
               name="email"
+              value={email}
               placeholder="your-email@mail.com"
               required
+              onChange={this.handleEmailChange}
             />
           </label>
           <label htmlFor="" className={(formLabel, agreementBox)}>
-            <input type="checkbox" name="agreement" required />
+            <input
+              type="checkbox"
+              name="agreement"
+              required
+              checked={isCheked}
+              onChange={this.handleChecking}
+            />
             <span className={labelName}>
               I agree with all the rules of this service
             </span>
